4810865d4ecc1a5ed5c3b933dbf8a1842ac0da24
dovesandy <dovesandy@outlook.com>
2026-07-10 01:02:56 +0800

    公务员-B类

--- Android/网课学习/ActivityTest/app/src/main/java/com/lingchongsen/activitytest/FirstActivity.java
+++ Android/网课学习/ActivityTest/app/src/main/java/com/lingchongsen/activitytest/FirstActivity.java
@@ -1,6 +1,12 @@
 package com.lingchongsen.activitytest;
 
+import static com.lingchongsen.activitytest.R.*;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +19,18 @@ public class FirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(id.button_1);
+        if (button1 == null) {
+            Log.e("FirstActivity", "Button not found!");
+        } else {
+            Log.d("FirstActivity", "Button found");
+            button1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
